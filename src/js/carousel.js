075d2e87c0325319a195b671dd485df50af52cb4
Stein, Jeff <jdstein1@gmail.com>
2016-10-10 16:49:44 -0500

    work on auto scroll.

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -2,8 +2,19 @@
 
 var carousel = document.getElementsByClassName("carousel")[0], 
   carouselList = document.getElementsByClassName("carousel-list")[0], 
-  carouselItems = document.getElementsByClassName("carousel-item");
+  carouselItems = document.getElementsByClassName("carousel-item"), 
+  carouselItemWidth = carousel.clientWidth;
 
 console.log('carousel: ', carousel);
+console.log('carousel: ', carousel.clientWidth);
+
 console.log('carouselList: ', carouselList);
+console.log('carouselList: ', carouselList.clientWidth);
+
 console.log('carouselItems: ', carouselItems);
+console.log('carouselItems: ', carouselItems[0].clientWidth);
+
+/* AUTOMATICALLY SCROLL CAROUSEL */
+var autoscroll = function () {
+  console.log('START autoscroll');
+};
