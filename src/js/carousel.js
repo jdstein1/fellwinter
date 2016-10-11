90a90f6d507a0e5da1a5b28edc67e2115fc24271
Stein, Jeff <jdstein1@gmail.com>
2016-10-11 10:36:39 -0500

    add choppy auto scrolling and scroll control.

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -3,18 +3,52 @@
 var carousel = document.getElementsByClassName("carousel")[0], 
   carouselList = document.getElementsByClassName("carousel-list")[0], 
   carouselItems = document.getElementsByClassName("carousel-item"), 
-  carouselItemWidth = carousel.clientWidth;
+  scrollControl = document.getElementById("scrollControl"), 
+  carouselItemWidth = carousel.clientWidth, 
+  carouselListWidth = carouselList.clientWidth;
 
 console.log('carousel: ', carousel);
-console.log('carousel: ', carousel.clientWidth);
+console.log('carouselItemWidth: ', carouselItemWidth);
 
 console.log('carouselList: ', carouselList);
-console.log('carouselList: ', carouselList.clientWidth);
+console.log('carouselListWidth: ', carouselListWidth);
 
 console.log('carouselItems: ', carouselItems);
-console.log('carouselItems: ', carouselItems[0].clientWidth);
+
 
 /* AUTOMATICALLY SCROLL CAROUSEL */
-var autoscroll = function () {
-  console.log('START autoscroll');
+
+var intervalID, 
+  scrollingFlag = false;
+
+var autoScroll = function () {
+  console.log('START autoScroll');
+  intervalID = window.setInterval(doScroll, 1000);
 };
+
+function doScroll() {
+  console.log('START doScroll: '+carousel.scrollLeft);
+  scrollingFlag = true;
+  if (carousel.scrollLeft >= (carouselListWidth-carouselItemWidth)) {
+    carousel.scrollLeft = 0;
+  } else {
+    carousel.scrollLeft += carouselItemWidth;
+  }
+}
+
+function stopScroll() {
+  console.log('START stopScroll');
+  if (scrollingFlag === true) {
+    scrollControl.innerHTML = "Resume";
+    window.clearInterval(intervalID);
+    scrollingFlag = false;
+  } else {
+    scrollControl.innerHTML = "Stop Again";
+    autoScroll();
+  }
+}
+
+// window.setTimeout(function() {
+//     alert("Hello World!");
+// }, 500);
+
