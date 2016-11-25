074b374da6a4074fae5307c8d4e8ca4ddc933728
Stein, Jeff <jstein0@searshc.com>
2016-11-25 16:21:36 -0600

    add 'gt_' namespacing to gulp tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var plugins = require('gulp-load-plugins')();
 
 // Load plugins manually...
 var wiredep = require('wiredep').stream,
-  browser = require('browser-sync').create(),
+  browserSync = require('browser-sync').create(),
   copy = require('copy'),
   del = require('del'),
   pump = require('pump');
@@ -29,10 +29,10 @@ var paths = {
   }
 };
 
-// browser:src for dev task
-gulp.task('browser:src', function() {
-    console.log('START browser:src');
-    browser.init({
+// gt_bs:src for dev task
+gulp.task('gt_bs:src', function() {
+    console.log('START gt_bs:src');
+    browserSync.init({
         server: {
             baseDir: conf.dirs.src,
             index: conf.files.index
@@ -48,16 +48,16 @@ gulp.task('browser:src', function() {
     gulp.watch(conf.files.css, function () {
       console.log('change to css');
     });
-    // gulp.watch(conf.files.html).on('change', browser.reload);
+    // gulp.watch(conf.files.html).on('change', browserSync.reload);
     gulp.watch(conf.files.html, function () {
       console.log('change to html');
-    }).on('change', browser.reload);
+    }).on('change', browserSync.reload);
 });
 
-// browser:dest for default task
-gulp.task('browser:dest', function() {
-    console.log('START browser:dest');
-    browser.init({
+// gt_bs:dest for default task
+gulp.task('gt_bs:dest', function() {
+    console.log('START gt_bs:dest');
+    browserSync.init({
         server: {
             baseDir: conf.dirs.dest,
             index: conf.files.index
@@ -73,19 +73,19 @@ gulp.task('browser:dest', function() {
     gulp.watch(conf.files.css, function () {
       console.log('change to css');
     });
-    // gulp.watch(conf.files.html).on('change', browser.reload);
+    // gulp.watch(conf.files.html).on('change', browserSync.reload);
     gulp.watch(conf.files.html, function () {
       console.log('change to html');
-    }).on('change', browser.reload);
+    }).on('change', browserSync.reload);
 });
 
-gulp.task('clean', function() {
+gulp.task('gt_clean', function() {
   return del([
     conf.dirs.dest+'*',conf.dirs.src+'**/*.min.*'
   ])
 });
 
-gulp.task('jshint', function() {
+gulp.task('gt_hint', function() {
   return gulp
     .src([
       conf.dirs.src + conf.files.js,
@@ -100,7 +100,7 @@ gulp.task('jshint', function() {
 //   console.log('error', error);
 // }
 
-gulp.task('min-js', function(errors) {
+gulp.task('gt_minjs', function(errors) {
   console.log('paths.src.js: ', paths.src.js);
   pump([
     gulp.src(paths.src.js),
@@ -116,7 +116,7 @@ gulp.task('min-js', function(errors) {
   ], errors);
 });
 
-gulp.task('min-css', function(errors) {
+gulp.task('gt_mincss', function(errors) {
   pump([
     gulp.src(paths.src.css),
     plugins.concat('app.concat.css', {newLine:'\r\n'}),
@@ -130,7 +130,7 @@ gulp.task('min-css', function(errors) {
   ], errors);
 });
 
-gulp.task('copy', function() {
+gulp.task('gt_copy', function() {
   copy(
     [
       './src/*.html',
@@ -150,22 +150,25 @@ gulp.task('copy', function() {
   )
 });
 
-gulp.task('wiredeps', function() {
+gulp.task('gt_deps', function() {
   return gulp
     .src(paths.src.index)
     .pipe(wiredep())
     .pipe(gulp.dest(conf.dirs.dest));
 });
 
-gulp.task('show', function() {
+gulp.task('gt_show', function() {
   console.log('plugins: ', plugins);
+  // for (var i = 0; i < plugins.length; i++) {
+  //   console.log('plugins['+i+']: ', plugins[i]);
+  // }
 });
 
 // Minification Task
-gulp.task('min', [ 'min-css', 'min-js' ]);
+gulp.task('min', [ 'gt_mincss', 'gt_minjs' ]);
 
 // DEV Task 
-gulp.task('dev', [ 'clean', 'jshint', 'wiredeps', 'browser:src' ]);
+gulp.task('dev', [ 'gt_clean', 'gt_hint', 'gt_deps', 'gt_bs:src' ]);
 
 // Default PROD Task
-gulp.task('default', [ 'clean', 'jshint', 'min', 'wiredeps', 'browser:dest' ]);
+gulp.task('default', [ 'gt_clean', 'gt_hint', 'min', 'gt_deps', 'gt_copy', 'gt_bs:dest' ]);
